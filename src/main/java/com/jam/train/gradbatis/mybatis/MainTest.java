2f8272dbe82f83dc77ca65fcf57048bfb8458a11
tangjin <lucktangjin@163.com>
2019-07-30 22:50:02 +0800

    '新建表'

--- src/main/java/com/jam/train/gradbatis/mybatis/MainTest.java
+++ src/main/java/com/jam/train/gradbatis/mybatis/MainTest.java
@@ -1,7 +1,7 @@
 package com.jam.train.gradbatis.mybatis;
 
-import com.jam.train.gradbatis.mybatis.domain.Admin;
-import com.jam.train.gradbatis.mybatis.mapper.AdminMapper;
+import com.jam.train.gradbatis.mybatis.domain.Country;
+import com.jam.train.gradbatis.mybatis.mapper.CountryMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -24,18 +24,13 @@ public class MainTest {
             SqlSession session = sqlSessionFactory.openSession();
 
             // 获得xml映射文件中定义的操作语句
-//            Admin p = session.selectOne("com.jam.train.gradbatis.mybatis.mapper.AdminMapper.selectByPrimaryKey", 1);
-//            System.out.println(p);
-//            System.out.println(p.getUsername());
+            Country c = session.selectOne("com.jam.train.gradbatis.mybatis.mapper.CountryMapper.selectByPrimaryKey", 1);
+            System.out.println(c.getCountrycode());
 
             // 获得mapper接口的代理对象
-            AdminMapper pm = session.getMapper(AdminMapper.class);
-            // 直接调用接口的方法，查询id为1的Peson数据
-            Admin p2 = pm.selectByPrimaryKey(1);
-            // 打印Peson对象
-            System.out.println(p2);
-            // 打印Person对象关联的Card对象
-            System.out.println(p2.getUsername());
+            CountryMapper countryMapper = session.getMapper(CountryMapper.class);
+            Country country = countryMapper.selectByPrimaryKey(1);
+            System.out.println(country.getCountryname());
 
             // 提交事务
             session.commit();
---
+++ src/test/java/com/jam/train/gradbatis/mybatis/CountryMapperTest.java
@@ -0,0 +1,71 @@
+package com.jam.train.gradbatis.mybatis;
+
+import com.jam.train.gradbatis.mybatis.domain.Country;
+import com.jam.train.gradbatis.mybatis.domain.SysUser;
+import com.jam.train.gradbatis.mybatis.mapper.CountryMapper;
+import com.jam.train.gradbatis.mybatis.mapper.SysUserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+public class CountryMapperTest  {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+
+    @BeforeClass
+    public static void init(){
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void findOne(){
+       SqlSession sqlSession =  sqlSessionFactory.openSession();
+       try{
+           Country country =  sqlSession.selectOne("com.jam.train.gradbatis.mybatis.mapper.CountryMapper.selectByPrimaryKey",1);
+           System.out.println(country.getCountryname());
+       }finally {
+           sqlSession.close();
+       }
+    }
+
+    @Test
+    public void findOne2(){
+        SqlSession sqlSession =  sqlSessionFactory.openSession();
+        try{
+            CountryMapper countryMapper = sqlSession.getMapper(CountryMapper.class);
+            Country country = countryMapper.selectByPrimaryKey(1);
+            System.out.println(country.getCountryname());
+
+
+            SysUserMapper sysUserMapper = sqlSession.getMapper(SysUserMapper.class);
+            SysUser user = sysUserMapper.selectByPrimaryKey(1L);
+            System.out.println(user.getUserName());
+
+
+        }finally {
+            sqlSession.close();
+        }
+    }
+
+
+    private void printList(List<Country> countryList){
+        for(Country country:countryList){
+            System.out.println(country.getCountryname());
+        }
+    }
+
+}
